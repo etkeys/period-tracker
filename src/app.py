1b3788a9d6aa5c72d1412986e747467e458db928
E. Keys <e.t.keys@gmail.com>
2023-08-31 19:37:13 -0400

    Add some initial database stuff

--- src/app.py
+++ src/app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template, flash, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'develop'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(254), unique=True, nullable=False)
+    image_file = db.Column(db.String(100), nullable=False, default='default_profile.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    cycles = db.relationship('Cycle', backref='person', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.email}', '{self.image_file}')"
+
+
+class Cycle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Cycle('{self.user_id}', '{self.start_date}', '{self.end_date}')"
 
 @app.route('/')
 def home():
