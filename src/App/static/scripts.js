e3db1805f3fe73130503b870a6196d74a675dee8
E. Keys <e.t.keys@gmail.com>
2023-09-07 18:42:49 -0400

    wip: navbar mobile improvments

--- src/App/static/scripts.js
+++ src/App/static/scripts.js
@@ -1,4 +1,14 @@
 
+function clickToggleBlockDisplay(elementId){
+    var e = document.getElementById(elementId);
+    if (e.style.display == "none"){
+        e.style.display = "block";
+    }
+    else{
+        e.style.display = "none";
+    }
+}
+
 function clickToggleDropdown(elementId) {
     var e = document.getElementById(elementId);
     if (e.className.indexOf("w3-show") == -1){
